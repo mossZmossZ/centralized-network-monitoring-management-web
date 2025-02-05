881263121a5ce6dbd975bd8c4cf4a3f4d82ee610
Nattavee Narischat <nattavee123vee@gmail.com>
2025-02-05 22:09:30 +0700

    Update navbar auto hide function

--- frontend/components/navbar/index.jsx
+++ frontend/components/navbar/index.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const featureRef = useRef(null);
+  const menuRef = useRef(null);
+
+  // Function to close all dropdowns when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        featureRef.current && !featureRef.current.contains(event.target) &&
+        menuRef.current && !menuRef.current.contains(event.target)
+      ) {
+        featureRef.current.removeAttribute("open");
+        menuRef.current.removeAttribute("open");
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -12,16 +34,20 @@ export default function Navbar() {
           <li><Link to="/alert">Alert</Link></li>
           <li><Link to="/systems">System</Link></li>
           <li><Link to="/setting">Setting</Link></li>
+          
+          {/* Feature Dropdown */}
           <li>
-            <details>
+            <details ref={featureRef}>
               <summary>Feature</summary>
               <ul className="bg-base-100 rounded-t-none p-2">
                 <li><Link to="/docs">Password leak</Link></li>
               </ul>
             </details>
           </li>
+
+          {/* Menu Dropdown */}
           <li>
-            <details>
+            <details ref={menuRef}>
               <summary>Menu</summary>
               <ul className="bg-base-100 rounded-t-none p-2">
                 <li><Link to="/user">User</Link></li>
